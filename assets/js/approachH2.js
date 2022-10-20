c2ee3d86ab616869f6a9e72215a87ccb260d90b4
naspitha <naspitha@users.noreply.github.com>
2022-10-21 01:24:01 +0200

    added legend and other text elements

--- assets/js/approachH2.js
+++ assets/js/approachH2.js
@@ -1,7 +1,7 @@
 //TODO: make size of graph responsive
 //TODO: zoom
 
-var margin = {top: 10, right: 10, bottom: 10, left: 10}
+var margin = {top: 10, right: 10, bottom: 15, left: 10}
 var graphDiv = document.getElementById("approachH2")
 
 var width = graphDiv.clientWidth - margin.left - margin.right
@@ -13,8 +13,8 @@ var titleHeight = height*0.06
 var distanceValue = document.getElementById('distanceValue')
 var distanceSlider = document.getElementById("distance")
 
-var rSelected = 3; distanceValue.innerHTML = rSelected.toFixed(1)
-var rMin = 0.1, rMax = 8, rStep=0.05
+var rSelected = 4; distanceValue.innerHTML = rSelected.toFixed(1)
+var rMin = 0.1, rMax = 12, rStep=0.05
 var rDrawMin = document.getElementById("distance").min
 var rList = arange(rMin, rMax, rStep)
 var rData = [{"r":0}]
@@ -23,7 +23,9 @@ for (i=1; i<rList.length; i++){
 }
 
 // color scheme
-
+bondingColor = "rgb(150, 122, 150)"
+antibondingColor = "rgb(240, 120, 0)"
+axisColor = "rgb(120, 120, 120)"
 
 // append the svg object to the body of the page
 var svg = d3.select("#approachH2")
@@ -45,42 +47,59 @@ screenMO.append("rect")
     // .style("stroke", "#585858")
 
 // Potential Energy Graph screen
-var graphWidth = width*0.65-dividerHalfWidth*2
-var plotWidth = graphWidth*0.80
-var graphHeight = height*0.4-dividerHalfWidth
-var plotHeight = graphHeight*0.85
+var graphLeft = margin.left+width*0.12
+var graphWidth = width*0.5-dividerHalfWidth*4
+var graphTop = margin.top + height*0.55 + dividerHalfWidth
+var graphHeight = height*0.38-dividerHalfWidth-margin.bottom
 
 var screenPEG = svg.append("g").attr("id", "screenPEG")
 var graphPEG = screenPEG.append("rect")
-    .attr("x", margin.left+graphWidth-plotWidth)
-    .attr("y", margin.top + height*0.6+ dividerHalfWidth)
-    .attr("width", plotWidth)
-    .attr("height", plotHeight)
+    .attr("x", graphLeft)
+    .attr("y", graphTop)
+    .attr("width", graphWidth)
+    .attr("height", graphHeight)
     .style("fill", "white")
     // .style("stroke", "#585858")
 
 // PEG Screen
 var rScale = d3.scaleLinear()
     .domain([0, 6])
-    .range([+graphPEG.attr("x"),+graphPEG.attr("x")+(+graphPEG.attr("width"))])
+    .range([graphLeft,graphLeft+graphWidth])
 var eScale = d3.scaleLinear()
     .domain([-17, -10])
-    .range([+graphPEG.attr("y")+(+graphPEG.attr("height")), +graphPEG.attr("y")])
+    .range([graphTop+graphHeight, graphTop])
 var xAxisPEG = screenPEG.append("g")
     .call(d3.axisBottom(rScale))
-    .attr("transform", "translate(0, "+ (+graphPEG.attr("y")+ (+graphPEG.attr("height")))+ ")")
+    .attr("transform", "translate(0, "+ (graphTop+graphHeight)+ ")")
     .attr("color", "#585858")
+screenPEG.append("text").text("Abstand (Å)")
+    .attr("x", graphLeft+graphWidth/2)
+    .attr("y", height-margin.bottom)
+    .attr("text-anchor", "middle")
+    .attr("alignment-baseline", "central")
+    .style("font-size", "0.7em")
+var yLabelX = graphLeft/2, yLabelY = graphTop+graphHeight/2
+screenPEG.append("text").text("Energie (eV)")
+    .attr("x", yLabelX)
+    .attr("y", yLabelY)
+    .attr("text-anchor", "middle")
+    .attr("alignment-baseline", "middle")
+    .style("font-size", "0.7em")
+    .attr("transform", "rotate(-90,"+(yLabelX)+","+(yLabelY)+")")
+
 var yAxisPEG = screenPEG.append("g")
     .call(d3.axisLeft(eScale))
-    .attr("transform", "translate("+ (+graphPEG.attr("x"))+ ", 0)")
+    .attr("transform", "translate("+ graphLeft+ ", 0)")
+
+
 
 // Styling axes
 var axes = [xAxisPEG, yAxisPEG]
 axes.forEach(function(axis){
-    axis.selectAll("path").style("stroke", "#585858").style("stroke-width", "1")
-    axis.selectAll("line").style("stroke", "#585858").style("stroke-width", "1")
-    axis.selectAll(".tick line").style("stroke", "#585858").style("stroke-width", "1")
-    axis.selectAll("text").style("fill", "#585858").style("font-size", "0.8em")
+    axis.selectAll("path").style("stroke", axisColor).style("stroke-width", "1")
+    axis.selectAll("line").style("stroke", axisColor).style("stroke-width", "1")
+    axis.selectAll(".tick line").style("stroke", axisColor).style("stroke-width", "1")
+    axis.selectAll("text").style("fill", axisColor).style("font-size", "0.7em")
 })
 
 // Adding indicator
@@ -95,9 +114,22 @@ screenPEG.append("text").attr("id", "indPEG-text")
     .text(`r: ${rSelected.toFixed(1)} Å`)
     .style("fill", "#74A9D8").style("font-size", "0.8em")
 function moveCursor(r){
-    d3.select("#indPEG-line").attr("x1", rScale(r)).attr("x2", rScale(r))
-    d3.select("#indPEG-text").attr("x", +d3.select("#indPEG-line").attr("x1")+lineToLabel)
+    d3.select("#indPEG-line").attr("x1", function(){
+        if(r<rScale.domain()[1]){
+            return rScale(r)} else {return graphLeft+graphWidth}})
+    .attr("x2", function(){
+        if(r<rScale.domain()[1]){
+            return rScale(r)} else {return graphLeft+graphWidth}})
+    .attr("opacity", function(){if (r<rScale.domain()[1]){
+        return 1} else { return 0.2}})
+    d3.select("#indPEG-text").attr("x", function(){
+        if (r<rScale.domain()[1]){ 
+            return +d3.select("#indPEG-line").attr("x1")+lineToLabel
+        } else {return graphLeft+graphWidth+lineToLabel}
+    })
         .text(`r: ${r.toFixed(2)} Å`)
+    d3.select("#es1g-label").text("E(σ) : "+(MOLvls(r)[0]).toFixed(2)+" eV")
+    d3.select("#es1u-label").text("E(σ*) : "+(MOLvls(r)[1]).toFixed(2)+" eV")
 }
 
 dataEs1g = rData.filter((d)=>d.r <rScale.domain()[1] && MOLvls(d.r)[0] < eScale.domain()[1])
@@ -106,8 +138,8 @@ dataEs1u = rData.filter((d)=>d.r <rScale.domain()[1] && MOLvls(d.r)[1] < eScale.
 screenPEG.append("path")
     .datum(dataEs1g)
     .attr("fill", "none")
-    .attr("stroke", "rgb(180, 120, 0)")
-    .attr("stroke-width", 1.5)
+    .attr("stroke", bondingColor)
+    .attr("stroke-width", "0.1em")
     .attr("id", "es1g")
     .attr("d", d3.line()
         .x(function(d) { return rScale(d.r) })
@@ -116,17 +148,34 @@ screenPEG.append("path")
 screenPEG.append("path")
     .datum(dataEs1u)
     .attr("fill", "none")
-    .attr("stroke",  "rgb(70,150,0)")
-    .attr("stroke-width", 1.5)
+    .attr("stroke",  antibondingColor)
+    .attr("stroke-width", "0.1em")
     .attr("id", "es1u_graph")
     .attr("d", d3.line()
         .x(function(d) { return rScale(d.r) })
         .y(function(d) { return eScale(MOLvls(d.r)[1]) })
         )
-
-screenPEG.append("rect")
-    .attr("x", rScale(4.6))
-
+// Legend
+var legend = screenPEG.append("g")
+    .attr("transform", "translate("+(graphLeft+0.60*graphWidth) +","+(graphTop+0.03*graphWidth) +")")
+legend.append("rect").attr("id", "legendBox")
+    .attr("x", 0).attr("width", graphWidth*0.37)
+    .attr("y", 0).attr("height", graphHeight*0.25)
+    .style("fill", "white").style("stroke", "#74A9D8")
+    .style("stroke-width", "0.01em")
+var legendHeight  = d3.select("#legendBox").attr("height")
+legend.append("circle")
+    .attr("cx", legendHeight*0.45).attr("cy", legendHeight*0.25).attr("r", legendHeight*0.1).style("fill", bondingColor)
+legend.append("text").attr("id", "es1g-label")
+    .attr("x", legendHeight*0.62).attr("y", legendHeight*0.25)
+    .style("alignment-baseline", "middle").style("font-size", "0.8em")
+    .text("E(σ) : "+(MOLvls(rSelected)[0]).toFixed(2)+" eV")
+legend.append("circle")
+    .attr("cx", legendHeight*0.45).attr("cy", legendHeight*0.7).attr("r", legendHeight*0.1).style("fill", antibondingColor)
+legend.append("text").attr("id", "es1u-label")
+    .attr("x", legendHeight*0.62).attr("y", legendHeight*0.7)
+    .style("alignment-baseline", "middle").style("font-size", "0.8em")
+    .text("E(σ*) : "+(MOLvls(rSelected)[1]).toFixed(2)+" eV")
 // s1u orbital screen
 var screenS1u = svg.append("g").attr("id", "screenS1u")
 var screenS1uRect = screenS1u.append("rect").attr("id", "screen1uRect")
@@ -135,7 +184,7 @@ var screenS1uRect = screenS1u.append("rect").attr("id", "screen1uRect")
     .attr("width", width*0.35-dividerHalfWidth)
     .attr("height", width*0.35-dividerHalfWidth)
     .style("fill", "#585858")
-    .style("stroke",  "rgb(70,150,0)").style("stroke-width", "0.5em")
+    .style("stroke",  antibondingColor).style("stroke-width", "1em")
 
 // s1g orbital screen
 var screenS1g = svg.append("g").attr("id", "screenS1g")
@@ -145,41 +194,54 @@ var screenS1gRect = screenS1g.append("rect").attr("id", "screen1gRect")
     .attr("width", width*0.35-dividerHalfWidth)
     .attr("height", width*0.35-dividerHalfWidth)
     .style("fill", "#585858")
-    .style("stroke", "rgb(180, 120, 0)").style("stroke-width", "0.5em")
-    
+    .style("stroke", bondingColor).style("stroke-width", "1em")
+
+var orbitalScreensX = +screenS1uRect.attr("x") 
+var orbitalScreensS1uY = +screenS1uRect.attr("y")
+var orbitalScreensS1gY = +screenS1gRect.attr("y")
+var orbitalScreensSide = +screenS1uRect.attr("width")
+svg.append("text").text("ψ (σ*)")
+    .attr("x", orbitalScreensX+orbitalScreensSide*0.1).attr("y", orbitalScreensS1uY+orbitalScreensSide*0.1)
+    .style("fill", "white").style("font-size", "1.7em").style("font-style", "italic")
+    .style("font-family", "serif")
+svg.append("text").text("ψ (σ)")
+    .attr("x", orbitalScreensX+orbitalScreensSide*0.1).attr("y", orbitalScreensS1gY+orbitalScreensSide*0.1)
+    .style("fill", "white").style("font-size", "1.7em").style("font-style", "italic")
+    .style("font-family", "serif")
 
 // drawing Orbitals
 var rScaleMORight = d3.scaleLinear()
     .domain([rDrawMin, rMax])
-    .range([screenMOx+screenMOWidth*0.65, screenMOx+screenMOWidth*0.9])
+    .range([screenMOx+screenMOWidth*0.65, screenMOx+screenMOWidth*0.8])
 var rScaleMOLeft = d3.scaleLinear()
     .domain([rDrawMin, rMax])
-    .range([screenMOx+screenMOWidth*0.35, screenMOx+screenMOWidth*0.1])
+    .range([screenMOx+screenMOWidth*0.35, screenMOx+screenMOWidth*0.2])
 var eScaleMO = d3.scaleLinear()
     .domain([eScale.domain()[0], MOLvls(+rDrawMin)[1]])
     .range([screenMOy+screenMOHeight*0.9, screenMOy+screenMOHeight*0.1])
 
 let orbitalWidth = screenMOWidth*0.1
+// arrow
 svg.append("line")
-    .attr("x1", rScaleMOLeft(rMax)-orbitalWidth/2)
-    .attr("x2", rScaleMOLeft(rMax)-orbitalWidth/2)
+    .attr("x1", rScaleMOLeft(rMax*1.3)-orbitalWidth/2)
+    .attr("x2", rScaleMOLeft(rMax*1.3)-orbitalWidth/2)
     .attr("y1", eScaleMO(-16))
     .attr("y2", eScaleMO(-2))
     .style("stroke", "black")
 svg.append("line")
-    .attr("x1", rScaleMOLeft(rMax)-orbitalWidth/2-orbitalWidth/10)
-    .attr("x2", rScaleMOLeft(rMax)-orbitalWidth/2)
+    .attr("x1", rScaleMOLeft(rMax*1.3)-orbitalWidth/2-orbitalWidth/10)
+    .attr("x2", rScaleMOLeft(rMax*1.3)-orbitalWidth/2)
     .attr("y1", eScaleMO(-2.4))
     .attr("y2", eScaleMO(-2))
     .style("stroke", "black")
 svg.append("line")
-    .attr("x1", rScaleMOLeft(rMax)-orbitalWidth/2)
-    .attr("x2", rScaleMOLeft(rMax)-orbitalWidth/2+orbitalWidth/10)
+    .attr("x1", rScaleMOLeft(rMax*1.3)-orbitalWidth/2)
+    .attr("x2", rScaleMOLeft(rMax*1.3)-orbitalWidth/2+orbitalWidth/10)
     .attr("y1", eScaleMO(-2))
     .attr("y2", eScaleMO(-2.4))
     .style("stroke", "black")
 svg.append("text")
-    .attr("x", rScaleMOLeft(rMax)-orbitalWidth/2)
+    .attr("x", rScaleMOLeft(rMax*1.3)-orbitalWidth/2)
     .attr("y", eScaleMO(-1.5))
     .attr("text-anchor", "middle")
     .text("E").style("font-size", "1.3em")
@@ -202,7 +264,7 @@ var blockHeight = 2*rMax/resolutionY
 var pointsXY = []
 
 var wfColor = d3.scaleLinear()
-    .domain([-(Math.sqrt(1/(4*Math.PI))), 0, Math.sqrt(1/(4*Math.PI)), 2*Math.sqrt(1/(4*Math.PI))])
+    .domain([-2*Math.sqrt(1/(4*Math.PI)), 0, 2*Math.sqrt(1/(4*Math.PI)), 4*Math.sqrt(1/(4*Math.PI))])
     .range(["blue", "black", "red", "rgb(255, 120, 120)"]).interpolate(d3.interpolateRgb.gamma(2.2))
 
 for (i=0; i<resolutionY; i++){
@@ -221,7 +283,11 @@ screenS1u.selectAll("myPoints")
     .attr("width", xScaleS1u(blockWidth)-xScaleS1u(0)).attr("height", yScaleS1u(blockHeight)-yScaleS1u(0))
     .style("fill", function(d){
         let value = subtractWavefunctions([d.x, d.y], [-rSelected/2, 0], [rSelected/2, 0])
+        this.value  = value
         return wfColor(value)
+    })
+    .on("click", function(){
+        console.log(this.value)
     })
 
 screenS1g.selectAll("myPoints")
@@ -232,7 +298,11 @@ screenS1g.selectAll("myPoints")
     .attr("width", xScaleS1g(blockWidth)-xScaleS1g(0)).attr("height", yScaleS1g(blockHeight)-yScaleS1g(0))
     .style("fill", function(d){
         let value = addWavefunctions([d.x, d.y], [-rSelected/2, 0], [rSelected/2, 0])
+        this.value = value
         return wfColor(value)
+    })
+    .on("click", function(){
+        console.log(this.value)
     })
 
 function drawOrbitals(rDraw){
@@ -270,27 +340,29 @@ function drawOrbitals(rDraw){
             .attr("x2", screenMOx+screenMOWidth/2+orbitalWidth/2)
             .attr("y1", eScaleMO(MOEnergies[0]))
             .attr("y2", eScaleMO(MOEnergies[0]))
-            .style("stroke", "rgb(180, 120, 0)").style("stroke-width", "0.2em")
+            .style("stroke", bondingColor).style("stroke-width", "0.2em")
             .style("opacity", 0.8)
     screenMO.append("line")
             .attr("x1", screenMOx+screenMOWidth/2-orbitalWidth/2)
             .attr("x2", screenMOx+screenMOWidth/2+orbitalWidth/2)
             .attr("y1", eScaleMO(MOEnergies[1]))
             .attr("y2", eScaleMO(MOEnergies[1]))
-            .style("stroke", "rgb(70,150,0)").style("stroke-width", "0.2em")
+            .style("stroke", antibondingColor).style("stroke-width", "0.2em")
             .style("opacity", 0.8)
     screenMO.append("text")
             .attr("x", screenMOx+screenMOWidth/2)
             .attr("y", eScaleMO(MOEnergies[1])+orbitalToLabel)
             .attr("text-anchor", "middle")
-            .text("σ*").style("font-weight", 700)
-            .style("fill",  "rgb(70,150,0)").style("opacity", 0.8)
+            .text("σ*").style("font-weight", 700).style("font-style", "italic")
+            .style("font-family", "serif").style("font-size", "1.2em")
+            .style("fill",  antibondingColor).style("opacity", 0.8)
     screenMO.append("text")
             .attr("x", screenMOx+screenMOWidth/2)
             .attr("y", eScaleMO(MOEnergies[0])+orbitalToLabel)
             .attr("text-anchor", "middle")
-            .text("σ").style("font-weight", 700)
-            .style("fill", "rgb(180, 120, 0)").style("opacity", 0.8)
+            .text("σ").style("font-weight", 700).style("font-style", "italic")
+            .style("font-family", "serif").style("font-size", "1.2em")
+            .style("fill", bondingColor).style("opacity", 0.8)
 
     // connecting lines
     screenMO.append("line") // right to antibonding
