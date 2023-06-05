ada565519cc39f89278a8a4fb236e1b713b382ce
naspitha <naspitha@users.noreply.github.com>
2023-06-05 23:38:06 +0200

    ionChart MVP added

---
+++ other/ionNomenclature.js
@@ -0,0 +1,162 @@
+const ionNames = {
+    "cations": {
+        "K": {  "symbol": "K<sup>+</sup>",
+                "preName": "potassium",
+                "postName": "potassium",
+                "charge": 1,
+                "poly": false
+            },
+        "Na": {  "symbol": "Na<sup>+</sup>",
+                "preName": "sodium",
+                "postName": "sodium",
+                "charge": 1,
+                "poly": false
+        },
+        "Li": {  "symbol": "Li<sup>+</sup>",
+                "preName": "lithium",
+                "postName": "lithium",
+                "charge": 1,
+                "poly": false
+        },
+        "Be": {  "symbol": "Be<sup>2+</sup>",
+                "preName": "beryllium",
+                "postName": "beryllium",
+                "charge": 2,
+                "poly": false
+        },
+        "Ca": {  "symbol": "Ca<sup>2+</sup>",
+                "preName": "calcium",
+                "postName": "calcium",
+                "charge": 2,
+                "poly": false
+        },
+        "Mg": {  "symbol": "Mg<sup>2+</sup>",
+                "preName": "magnesium",
+                "postName": "magnesium",
+                "charge": 2,
+                "poly": false
+        },
+        "Ba": {  "symbol": "Ba<sup>2+</sup>",
+                "preName": "barium",
+                "postName": "barium",
+                "charge": 2,
+                "poly": false
+        },
+        "Pb": {  "symbol": "Pb<sup>2+</sup>",
+                "preName": "lead",
+                "postName": "lead",
+                "charge": 2,
+                "poly": false
+        },
+        "Al": {  "symbol": "Al<sup>3+</sup>",
+                "preName": "aluminum",
+                "postName": "aluminum",
+                "charge": 3,
+                "poly": false
+        },
+        "NH4": {  "symbol": "NH<sub>4</sub><sup>+</sup>",
+                "preName": "ammonium",
+                "postName": "ammonium",
+                "charge": 1,
+                "poly": true
+        },
+        "FeIII": {  "symbol": "Fe<sup>3+</sup>",
+                "preName": "iron (III)",
+                "postName": "ferric",
+                "charge": 3,
+                "poly": false
+        },
+        "FeII": {  "symbol": "Fe<sup>2+</sup>",
+                "preName": "iron (II)",
+                "postName": "ferrous",
+                "charge": 2,
+                "poly": false
+        },
+        "H": {  "symbol": "H<sup>+</sup>",
+                "preName": "hydrogen",
+                "postName": "acid",
+                "charge": 1,
+                "poly": false
+        }
+    },
+
+    "anions": {
+        "Cl": { "symbol": "Cl<sup>-</sup>",
+                "preName": "hydrochloric",
+                "postName": "chloride",
+                "charge": 1,
+                "poly": false,
+                "order": 1
+            },
+        "F": {  "symbol": "F<sup>-</sup>",
+                "preName": "hydrofluoric",
+                "postName": "fluoride",
+                "charge": 1,
+                "poly": false,
+                "order": 0
+        },
+        "Br": {  "symbol": "Br<sup>-</sup>",
+                "preName": "hydrobromic",
+                "postName": "bromide",
+                "charge": 1,
+                "poly": false,
+                "order": 2
+        },
+        "I": {  "symbol": "I<sup>-</sup>",
+                "preName": "hydroiodic",
+                "postName": "iodide",
+                "charge": 1,
+                "poly": false,
+                "order": 3
+        },
+        "NO3": {  "symbol": "NO<sub>3</sub><sup>-</sup>",
+                "preName": "nitric",
+                "postName": "nitrate",
+                "charge": 1,
+                "poly": true,
+                "order": 4
+        },
+        "PO4": {  "symbol": "PO<sub>4</sub><sup>3-</sup>",
+                "preName": "phosphoric",
+                "postName": "phosphate",
+                "charge": 3,
+                "poly": true,
+                "order": 8
+        },
+        "SO4": {  "symbol": "SO<sub>4</sub><sup>2-</sup>",
+                "preName": "sulfuric",
+                "postName": "sulfate",
+                "charge": 2,
+                "poly": true,
+                "order": 6
+        },
+        "SO3": {  "symbol": "SO<sub>3</sub><sup>2-</sup>",
+                "preName": "sulfurous",
+                "postName": "sulfite",
+                "charge": 2,
+                "poly": true,
+                "order": 7
+        },
+        "ClO3": {  "symbol": "ClO<sub>3</sub><sup>-</sup>",
+                "preName": "chloric",
+                "postName": "chlorate",
+                "charge": 1,
+                "poly": true,
+                "order": 9
+        },
+        "OH": {  "symbol": "OH<sup>-</sup>",
+                "preName": "water",
+                "postName": "hydroxide",
+                "charge": 1,
+                "poly": true,
+                "order": 5
+        },
+        "H": {  "symbol": "H<sup>-</sup>",
+                "preName": "water",
+                "postName": "hydride",
+                "charge": 1,
+                "poly": false,
+                "order": 10
+        }
+    }
+}
